2a7b25ba04f5b903129625c81eac6e93845fdde5
akashdeb <akashdeb87@gmail.com>
2024-02-24 12:56:25 +0530

    conflict

--- demoApps/src/test/java/dataDrivenTesting/Democlass.java
+++ demoApps/src/test/java/dataDrivenTesting/Democlass.java
@@ -5,6 +5,8 @@ public class Democlass {
 	public static void main(String[] args) {
 		
 		System.out.println("This is a demo class");
+		
+		System.out.println("This added by TE 1 after vacation");
 
 	}
 
